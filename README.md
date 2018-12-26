eaa1bcb6f7e9a784c8b8b87d2a72a3617a7a02cf
Kurt L. Manion <klmanion@gmail.com>
2018-12-26 05:37:50 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# tarot
+Display a Tarot card reading.
+
+The ASCII representations of the cards are highly configurable.
+Spreads are programmable through a light and flexible grammer.
---
+++ src/main.c
@@ -0,0 +1,18 @@
+/*-
+ *main.c
+ * tarot
+ * created by: Kurt L. Manion
+ * on: Wed., 26 Dec. 2018
+ */
+
+#include <stdlib.h>
+
+int
+main(
+    int argc,
+    char *argv[])
+{
+	return EXIT_SUCCESS;
+}
+
+/* vim: set ts=8 sw=8 noexpandtab tw=79: */
